dad5556182217d05dc2b5b69265aa74235574376
Alexandra Escandor <39238357+escandor@users.noreply.github.com>
2019-06-17 11:19:49 +0100

    Update myshell.py

--- myshell.py
+++ myshell.py
@@ -4,17 +4,14 @@ from shell_commands import MyShell
 def main():
 	prompt = MyShell()
 
-	if len(sys.argv) == 1: # if there is no batchfile
+	if len(sys.argv) == 1:	# if there is no batchfile
 		prompt.myprompt()
 	else:
 		batchfile = sys.argv[1]
-		with open(batchfile, "r") as f:
-			prompt.cmdqueue = [line.strip() for line in f.readlines()]
+		with open(batchfile, "r") as file:
+			prompt.cmdqueue = [line.strip() for line in file]
 			prompt.cmdqueue.append("quit")
-	print()
 	prompt.cmdloop()
-
-	#prompt.cmdloop("{}\nWelcome to your Shell!\n{}".format("\033[93;01m", "\033[0m"))
 
 if __name__ == '__main__':
 	main()
